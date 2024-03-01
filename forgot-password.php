e265fa162aed294542d2377246e1ba437619b443
Bello-Ibrahm <Ibrahimbello1243@gmail.com>
2024-03-01 21:12:52 +0100

    Add forgot password file

---
+++ forgot-password.php
@@ -0,0 +1,48 @@
+<?php
+include("includes/header.php");
+?>
+
+  <div class="container">
+
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
+      <div class="col-md-8 my-5">
+        <div class="card o-hidden border-0 shadow-lg my-5">
+          <div class="card-body p-0">
+            <div class="row">
+              <div class="col-md-2"></div>
+              <div class="col-md-8">
+                <div class="p-5">
+                  <div class="mb-4">
+                    <h1 class="h4 text-center  text-gray-900">Forgot password</h1>
+                    <p class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit quidem aut, iusto veritatis odio ea?</p>
+                    <hr>
+                  </div>
+                  <form class="user" method="post">
+                    <div class="form-group">
+                      <input type="email" class="form-control form-control-user" id="exampleInputEmail" required aria-describedby="emailHelp" placeholder="Enter your email address">
+                    </div>
+                    <a href="#" class="btn btn-primary btn-user btn-block">
+                      Submit
+                    </a>
+                    <hr>
+                  </form>
+                  <div class="">
+                    <a class="small" href="index">Back</a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-2"></div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+<?php
+    include("includes/footer.php");
+?>
+
