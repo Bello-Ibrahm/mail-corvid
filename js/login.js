3ea0c6e5722dec9750149a92206b7574386a89de
Bello-Ibrahm <Ibrahimbello1243@gmail.com>
2024-03-01 21:15:24 +0100

    Implement code structure

---
+++ js/login.js
@@ -0,0 +1,29 @@
+/*   CHANGE CLASS SUBJECT INSTRUCTOR MODAL */
+// $(".btn-user").click(function() {
+//     let username = $("#username");
+//     let pwd = $("#pwd");
+//     let fm = new FormData();
+    
+//     fm.append("username", username);
+//     fm.append("pwd", pwd);
+    // $.ajax({
+    //   url: "ajax/change-teacher.ajax.php",
+    //   method: "POST",
+    //   data: dat,
+    //   cache: false,
+    //   contentType: false,
+    //   processData: false,
+    //   dataType: "html",
+    //   success: function(response) {
+    //     $("#staff_id").html(response);
+    //     $("#class_id").val(class_id);
+    //     $("#subject_id").val(subject_id);
+    //   },
+    //   error: function(XMLHttpRequest, textStatus, errorThrown) {
+    //     console.log(XMLHttpRequest);
+    //     console.log(textStatus);
+    //     console.log(errorThrown);
+    //   }
+    // })
+//   });
+  /*-------x---- CHANGE CLASS SUBJECT INSTRUCTOR MODA -------x----*/
