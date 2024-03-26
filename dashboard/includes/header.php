ecfc56e899f8d513693912b7284e3fc7b37e3d09
Bello-Ibrahm <Ibrahimbello1243@gmail.com>
2024-03-26 22:21:31 +0100

    Modify includes file

--- dashboard/includes/header.php
+++ dashboard/includes/header.php
@@ -1,3 +1,12 @@
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+  session_start();
+}
+
+include("../database/Database.php");
+include("code.php");
+include("auth.php");
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,15 +18,42 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>SB Admin 2 - 404</title>
+  <title>Mailcorvid | Dashboard</title>
+
+  <link rel="icon" href="../prototype-image/logo-no_bg.png" type="image/png" />
 
   <!-- Custom fonts for this template-->
   <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
+
   <!-- Custom styles for this template-->
   <link href="../css/sb-admin-2.min.css" rel="stylesheet">
 
+  <!-- Custom styles for this page -->
+  <link href="../vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+
+  <!-- include summernote css -->
+  <!-- <link href="../vendor/summernote-bs4.min.css" rel="stylesheet"> -->
+  <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" rel="stylesheet">
+
+  <!-- Sweet alert  -->
+  <script src="../js/sweetalert2.all.js"></script>
+  <!-- <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> -->
 </head>
 
 <body id="page-top">
+
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+
+    <?php include("./includes/sidebar.php"); ?>
+
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+      <!-- Main Content -->
+      <div id="content">
+
+        <?php include("./includes/navbar.php"); ?>
